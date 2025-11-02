fe16a3e56d8f794bffdf0bf48c4730bbc782bea9
robberyducky <emarcoe@unomaha.edu>
2025-11-02 13:43:35 -0700

    Update urls.py
    Updates for production and deployment

--- mavfinder/urls.py
+++ mavfinder/urls.py
@@ -24,4 +24,5 @@ if settings.DEBUG:
 
 urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
